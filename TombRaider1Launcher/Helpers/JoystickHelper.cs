6f094a82d26d7144a381eeeef636e5f27d2de683
Steven Jalabert <stevenstrike31@gmail.com>
2018-02-10 21:46:09 +0100

    Initial Commit

---
+++ TombRaider1Launcher/Helpers/JoystickHelper.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using SharpDX.DirectInput;
+
+namespace TombRaider1Launcher
+{
+    class JoystickHelper
+    {
+        private DirectInput directInput;
+        private Task pollingTask;
+        private Joystick joystick;
+        private JoystickDescriptor joystickDescriptor;
+        private Task getDInputJoysticksTask;
+
+        public const int A_BUTTON_OFFSET = 48;
+        public const int B_BUTTON_OFFSET = 49;
+        public const int BACK_BUTTON_OFFSET = 54;
+        public const int START_BUTTON_OFFSET = 55;
+        public const int LAP_BUTTON_OFFSET = -1;
+        public const int POV_BUTTONS_OFFSET = 32;
+
+        public const int POV_UP_BUTTON_VALUE = 0;
+        public const int POV_DOWN_BUTTONS_VALUE = 18000;
+        public const int POV_LEFT_BUTTONS_VALUE = 27000;
+        public const int POV_RIGHT_BUTTONS_VALUE = 9000;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="JoystickHelper"/> class.
+        /// </summary>
+        public JoystickHelper()
+        {
+            this.directInput = new DirectInput();
+
+            detectDevices();
+        }
+
+        /// <summary>
+        /// Detects the devices.
+        /// </summary>
+        private void detectDevices()
+        {
+            getDInputJoysticksTask = Task.Factory.StartNew(() =>
+            {
+                do
+                {
+                    joystickDescriptor = directInput.GetDevices(DeviceType.Gamepad, DeviceEnumerationFlags.AllDevices)
+                                  .Concat(directInput.GetDevices(DeviceType.Joystick, DeviceEnumerationFlags.AllDevices))
+                                  .Select(d => new JoystickDescriptor(d.InstanceGuid, d.InstanceName))
+                                  .FirstOrDefault();
+
+                    if (joystickDescriptor != null)
+                    {
+                        this.StartCapture(joystickDescriptor.InstanceGUID);
+                    }
+
+                    Task.Delay(500);
+                } while (joystickDescriptor == null);
+            });
+        }
+
+        /// <summary>
+        /// Starts the capture.
+        /// </summary>
+        /// <param name="joystickGuid">The joystick unique identifier.</param>
+        public void StartCapture(Guid joystickGuid)
+        {
+            this.joystick = new Joystick(directInput, joystickGuid);
+            this.joystick.Properties.BufferSize = 128;
+            this.joystick.Acquire();
+            this.PollJoystick();
+        }
+
+        /// <summary>
+        /// Stops the capture.
+        /// </summary>
+        /// <param name="stopNow">if set to <c>true</c> [stop now].</param>
+        public void StopCapture(bool stopNow = false)
+        {
+            if (this.getDInputJoysticksTask != null)
+            {
+                this.getDInputJoysticksTask.Wait(250);
+            }
+
+            if (this.pollingTask != null)
+            {
+                this.pollingTask.Wait(250); // you can give this Wait a timeout
+            }
+        }
+
+        /// <summary>
+        /// Polls the joystick.
+        /// </summary>
+        private void PollJoystick()
+        {
+            this.pollingTask = Task.Factory.StartNew(() =>
+            {
+                while (true)
+                {
+                    try
+                    {
+                        joystick.Poll();
+                        JoystickUpdate[] data = this.joystick.GetBufferedData();
+                        foreach (JoystickUpdate state in data.Where(IsRelevantUpdate))
+                        {
+                            // pressed down
+                            JoystickButtonPressedEventArgs args = new JoystickButtonPressedEventArgs();
+                            args.ButtonOffset = state.RawOffset;
+                            args.PovValue = state.Value;
+                            args.NamedOffset = state.Offset.ToString().Trim();
+                            args.TimeStamp = DateTime.Now;
+
+                            this.OnJoystickButtonPressed(args);
+                        }
+                    }
+                    catch (SharpDX.SharpDXException)
+                    {
+                        this.StopCapture(true);
+                        return;
+                    }
+
+                    Task.Delay(250);
+                }
+            });
+        }
+
+        /// <summary>
+        /// Determines whether [is relevant update] [the specified state].
+        /// </summary>
+        /// <param name="state">The state.</param>
+        /// <returns>
+        ///   <c>true</c> if [is relevant update] [the specified state]; otherwise, <c>false</c>.
+        /// </returns>
+        private bool IsRelevantUpdate(JoystickUpdate state)
+        {
+            return
+                state.Offset >= JoystickOffset.Buttons0 &&
+                state.Offset <= JoystickOffset.Buttons127 &&
+                state.Value == 128 ||
+                // POV XBOX ONE.
+                (state.RawOffset == 32 && (state.Value == POV_UP_BUTTON_VALUE || state.Value == POV_RIGHT_BUTTONS_VALUE || state.Value == POV_DOWN_BUTTONS_VALUE || state.Value == POV_LEFT_BUTTONS_VALUE)) ||
+                // POV DINPUT 
+                ((state.Offset.ToString() == "X" || state.Offset.ToString() == "Y") && (state.Value == 0 || state.Value == 65535))
+                ;
+        }
+
+        /// <summary>
+        /// Raises the <see cref="E:JoystickButtonPressed" /> event.
+        /// </summary>
+        /// <param name="e">The <see cref="JoystickButtonPressedEventArgs"/> instance containing the event data.</param>
+        protected virtual void OnJoystickButtonPressed(JoystickButtonPressedEventArgs e)
+        {
+            this.JoystickButtonPressed?.Invoke(this, e);
+        }
+
+        /// <summary>
+        /// Occurs when [joystick button pressed].
+        /// </summary>
+        public event EventHandler<JoystickButtonPressedEventArgs> JoystickButtonPressed;
+
+        protected virtual void OnJoystickLapButtonPressed(JoystickButtonPressedEventArgs e)
+        {
+            this.JoystickLapButtonPressed?.Invoke(this, e);
+        }
+
+        /// <summary>
+        /// Occurs when [joystick lap button pressed].
+        /// </summary>
+        public event EventHandler<JoystickButtonPressedEventArgs> JoystickLapButtonPressed;
+
+        protected virtual void OnJoystickStartButtonPressed(JoystickButtonPressedEventArgs e)
+        {
+            this.JoystickStartButtonPressed?.Invoke(this, e);
+        }
+
+        /// <summary>
+        /// Occurs when [joystick start button pressed].
+        /// </summary>
+        public event EventHandler<JoystickButtonPressedEventArgs> JoystickStartButtonPressed;
+    }
+
+    public class JoystickButtonPressedEventArgs : EventArgs
+    {
+        public int ButtonOffset { get; set; }
+        public int PovValue { get; set; }
+        public string NamedOffset { get; set; }
+        public DateTime TimeStamp { get; set; }
+    }
+
+    public class JoystickDescriptor
+    {
+        private Guid _instanceGUID;
+        public Guid InstanceGUID
+        {
+            get { return _instanceGUID; }
+            set { _instanceGUID = value; }
+        }
+
+        private string _instanceName;
+        public string InstanceName
+        {
+            get { return _instanceName; }
+            set { _instanceName = value; }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="JoystickDescriptor"/> class.
+        /// </summary>
+        /// <param name="instanceGUID">The instance unique identifier.</param>
+        /// <param name="instanceName">Name of the instance.</param>
+        public JoystickDescriptor(Guid instanceGUID, string instanceName)
+        {
+            this.InstanceGUID = instanceGUID;
+            this.InstanceName = instanceName;
+        }
+    }
+}
---
+++ TombRaider1Launcher/MainWindow.xaml.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Linq;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace TombRaider1Launcher
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, IComponentConnector
+    {
+        private JoystickHelper joystickHelper = new JoystickHelper();
+
+        public MainWindow()
+        {
+            this.InitializeComponent();
+
+            joystickHelper.JoystickLapButtonPressed += joystickHelper_JoystickLapButtonPressed;
+            joystickHelper.JoystickButtonPressed += joystickHelper_JoystickButtonPressed;
+        }
+
+        /// <summary>
+        /// Handles the Click event of the TR1_Button control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="RoutedEventArgs"/> instance containing the event data.</param>
+        private void TR1_Button_Click(object sender, RoutedEventArgs e)
+        {
+            joystickHelper.StopCapture();
+
+            startTR1();
+        }
+
+        /// <summary>
+        /// Handles the Click event of the TUB_Button control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="RoutedEventArgs"/> instance containing the event data.</param>
+        private void TUB_Button_Click(object sender, RoutedEventArgs e)
+        {
+            joystickHelper.StopCapture();
+
+            startTR1_UB();
+        }
+
+        /// <summary>
+        /// Handles the Click event of the exitButton control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="RoutedEventArgs"/> instance containing the event data.</param>
+        private void exitButton_Click(object sender, RoutedEventArgs e)
+        {
+            joystickHelper.StopCapture();
+
+            this.Close();
+        }
+
+        /// <summary>
+        /// Starts the tr1.
+        /// </summary>
+        private void startTR1()
+        {
+            try
+            {
+                Environment.CurrentDirectory = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory);
+                ProcessStartInfo startInfo = new ProcessStartInfo("tombati.exe");
+                this.Hide();
+                Process.Start(startInfo).WaitForExit();
+                this.Close();
+            }
+            catch (SystemException)
+            {
+                this.Show();
+                int num = (int)MessageBox.Show("tombati.exe not found, please check the installation directory.");
+            }
+        }
+
+        /// <summary>
+        /// Starts the tr1 ub.
+        /// </summary>
+        private void startTR1_UB()
+        {
+            try
+            {
+                Environment.CurrentDirectory = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory);
+                ProcessStartInfo startInfo = new ProcessStartInfo("tombub.exe");
+                this.Hide();
+                Process.Start(startInfo).WaitForExit();
+                this.Close();
+            }
+            catch (SystemException)
+            {
+                this.Show();
+                int num = (int)MessageBox.Show("tombub.exe not found, please check the installation directory.");
+            }
+        }
+
+        /// <summary>
+        /// Moves the focus.
+        /// </summary>
+        /// <param name="direction">The direction.</param>
+        private void MoveFocus(FocusNavigationDirection direction)
+        {
+            var request = new TraversalRequest(direction);
+            this.MoveFocus(request);
+        }
+
+        /// <summary>
+        /// Handles the PreviewKeyDown event of the Window control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="KeyEventArgs"/> instance containing the event data.</param>
+        private void Window_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Left:
+                    this.MoveFocus(FocusNavigationDirection.Previous);
+                    break;
+                case Key.Right:
+                    this.MoveFocus(FocusNavigationDirection.Next);
+                    break;
+                case Key.Escape:
+                    this.Close();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Handles the JoystickStartButtonPressed event of the joystickHelper control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="JoystickButtonPressedEventArgs"/> instance containing the event data.</param>
+        private void joystickHelper_JoystickStartButtonPressed(object sender, JoystickButtonPressedEventArgs e)
+        {
+            MessageBox.Show("joystickHelper_JoystickStartButtonPressed");
+        }
+
+        /// <summary>
+        /// Handles the JoystickLapButtonPressed event of the joystickHelper control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="JoystickButtonPressedEventArgs"/> instance containing the event data.</param>
+        private void joystickHelper_JoystickLapButtonPressed(object sender, JoystickButtonPressedEventArgs e)
+        {
+            MessageBox.Show("joystickHelper_JoystickLapButtonPressed");
+        }
+
+        /// <summary>
+        /// Handles the JoystickButtonPressed event of the joystickHelper control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="JoystickButtonPressedEventArgs"/> instance containing the event data.</param>
+        private void joystickHelper_JoystickButtonPressed(object sender, JoystickButtonPressedEventArgs e)
+        {
+            switch (e.ButtonOffset)
+            {
+                case JoystickHelper.A_BUTTON_OFFSET:
+                case JoystickHelper.START_BUTTON_OFFSET:
+                    Application.Current.Dispatcher.Invoke((Action)delegate
+                    {
+                        SendKeys.Send(Key.Enter);
+                    });
+                    break;
+                case JoystickHelper.BACK_BUTTON_OFFSET:
+                case JoystickHelper.B_BUTTON_OFFSET:
+                    Application.Current.Dispatcher.Invoke((Action)delegate
+                    {
+                        this.Close();
+                    });
+                    break;
+                case JoystickHelper.POV_BUTTONS_OFFSET:
+                    switch (e.PovValue)
+                    {
+                        case JoystickHelper.POV_UP_BUTTON_VALUE:
+                            Application.Current.Dispatcher.Invoke((Action)delegate
+                            {
+                                SendKeys.Send(Key.Up);
+                            });
+                            break;
+                        case JoystickHelper.POV_DOWN_BUTTONS_VALUE:
+                            Application.Current.Dispatcher.Invoke((Action)delegate
+                            {
+                                SendKeys.Send(Key.Down);
+                            });
+                            break;
+                        case JoystickHelper.POV_LEFT_BUTTONS_VALUE:
+                            Application.Current.Dispatcher.Invoke((Action)delegate
+                            {
+                                SendKeys.Send(Key.Left);
+                            });
+                            break;
+                        case JoystickHelper.POV_RIGHT_BUTTONS_VALUE:
+                            Application.Current.Dispatcher.Invoke((Action)delegate
+                            {
+                                SendKeys.Send(Key.Right);
+                            });
+                            break;
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+            switch(e.NamedOffset)
+            {
+                case "X":
+                    if(e.PovValue == 0)
+                    {
+                        // Left
+                        Application.Current.Dispatcher.Invoke((Action)delegate
+                        {
+                            SendKeys.Send(Key.Left);
+                        });
+                    }
+                    else if(e.PovValue == 65535)
+                    {
+                        // Right
+                        Application.Current.Dispatcher.Invoke((Action)delegate
+                        {
+                            SendKeys.Send(Key.Right);
+                        });
+                    }
+                    break;
+                case "Y":
+                    if (e.PovValue == 0)
+                    {
+                        // Up
+                        Application.Current.Dispatcher.Invoke((Action)delegate
+                        {
+                            SendKeys.Send(Key.Up);
+                        });
+                    }
+                    else if (e.PovValue == 65535)
+                    {
+                        // Down
+                        Application.Current.Dispatcher.Invoke((Action)delegate
+                        {
+                            SendKeys.Send(Key.Down);
+                        });
+                    }
+                    break;
+            }
+        }
+    }
+}
